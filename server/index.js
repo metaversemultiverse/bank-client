774999700d428f386fbea12294242c696d2d987c
Metaverse Multiverse <148008296+metaversemultiverse@users.noreply.github.com>
2024-02-21 05:07:14 -0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const { resolve } = require('path');
 const app = express();
 
 // If you need a backend, e.g. an API, add your custom backend-specific middleware here
-// app.use('/api', myApi);
+app.use('/api', myApi);
 
 // In production we need to pass these values in instead of relying on webpack
 setup(app, {
